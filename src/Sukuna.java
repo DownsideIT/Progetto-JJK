7a8c50531177cafd11fe18b040851d1a29cf6241
Tony <antoniobelfiore08@gmail.com>
2026-02-26 13:34:40 +0100

    Sukuna changes

--- src/Sukuna.java
+++ src/Sukuna.java
@@ -1,3 +1,21 @@
+import java.util.Random;
+
 public class Sukuna extends HealthChanges {
 
+    Random rand=new Random();
+    public int mossaSukuna;
+
+    mossaSukuna=rand.nextInt(3)+1;
+
+    public Sukuna(double health){
+        this.health=health;
+    }
+
+    public void moveset(){
+        switch(mossaSukuna){
+
+            case 1:
+                break;
+        }
+    }
 }
